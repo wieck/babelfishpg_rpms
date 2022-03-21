0da2677ca0ff2260a46324a92034fd2674fa6b04
Jan Wieck <jan@wi3ck.info>
2022-03-21 02:24:05 +0000

    Erase PostgreSQL if installed.

--- dnf-install-babelfish.sh
+++ dnf-install-babelfish.sh
@@ -3,6 +3,12 @@
 # To be run as user root on the database host
 # after creating the babelfishpg RPM repository
 
+dnf erase -y \
+	postgresql13-libs \
+	postgresql13 \
+	postgresql13-server \
+	postgresql13-contrib
+
 dnf install -y \
 	babelfishpg13-libs \
 	babelfishpg13 \
