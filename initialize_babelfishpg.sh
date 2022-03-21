b1bb7f82ddaa0f6ddbe56895f6ef29ef5835c24a
Jan Wieck <jan@wi3ck.info>
2022-03-21 02:28:45 +0000

    Add more parameter adjustments to postgresql.auto.conf

--- initialize_babelfishpg.sh
+++ initialize_babelfishpg.sh
@@ -11,9 +11,23 @@ echo ""
 echo "Creating $PGDATA/postgresql.auto.conf"
 cat >/$PGDATA/postgresql.auto.conf <<_EOF_
 listen_addresses = '*'
+max_connections = 256
+shared_buffers = 16GB
+work_mem = 16MB
+maintenance_work_mem = 64MB
+wal_level = logical
+checkpoint_timeout = 10min
+max_wal_size = 64GB
+min_wal_size = 16GB
+checkpoint_completion_target = 0.9
+checkpoint_warning = 5min
+tcp_keepalives_idle = 57
+tcp_keepalives_interval = 7
+tcp_keepalives_count = 9
 shared_preload_libraries = 'babelfishpg_tds'
 babelfishpg_tsql.database_name = 'babelfish'
 babelfishpg_tsql.migration_mode = 'multi-db'
+babelfishpg_tds.tds_debug_log_level = 0
 _EOF_
 
 echo "Adding private networks to $PGDATA/pg_hba.conf"
